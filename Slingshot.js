92e85e8af06b61c90f50c0ef2eaeed93f7fb9130
Alice Anderson <76773238+KatieJuniors@users.noreply.github.com>
2021-05-03 16:00:46 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,33 @@
+class Slingshot{
+
+constructor(bodyA,pointB) {
+
+    var options = {
+        bodyA: bodyA,
+        pointB: pointB,
+        stiffness: 0.04,
+        length: 10
+    }
+    this.sling=Matter.Constraint.create(options);
+    this.pointB=pointB
+
+    World.add(world,this.sling);
+}
+
+fly() {
+    this.sling.bodyA=null;
+
+}
+
+
+display() {
+
+  if(this.sling.bodyA) {
+  strokeWeight(2)
+  stroke("white");
+  line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.pointB.x,this.pointB.y);
+  }
+  
+}
+
+}
